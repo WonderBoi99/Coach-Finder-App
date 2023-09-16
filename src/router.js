5138288b136a728edadbf7a1af04eef8de5888e7
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2023-09-15 18:01:41 -0600

    registered routes to components and made app.vue

--- src/router.js
+++ src/router.js
@@ -1,16 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachList from './pages/coaches/CoachList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsList from './pages/requests/RequestsList.vue';
+import NotFound from './pages/NotFound.vue';
+import { NO } from '@vue/shared';
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/coaches'},
-        { path: '/coaches', component: null},
-        { path: '/coaches/:id', component: null, children: [
-            { path: 'contact', component: null},
+        { path: '/coaches', component: CoachList},
+        { path: '/coaches/:id', component: CoachDetail, children: [
+            { path: 'contact', component: ContactCoach},
         ]},
-        { path: '/register', component: null},
-        { path: '/requests', component: null},
-        { path: '/:notFound(.*)', component: null},
+        { path: '/register', component: CoachRegistration},
+        { path: '/requests', component: RequestsList},
+        { path: '/:notFound(.*)', component: NotFound},
     ],
 });
 
