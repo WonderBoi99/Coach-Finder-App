786f4816ee449236b6fd52e98a5d45b5498099bf
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2023-09-19 16:11:23 -0600

    added mutation and action  for register coach

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -2,7 +2,7 @@
     <section>
         <base-card>
             <h2>Register as a coach</h2>
-            <coach-form></coach-form>
+            <coach-form @save-data="saveData"></coach-form>
         </base-card>
     </section>
 </template>
@@ -12,7 +12,13 @@ import CoachForm from '@/components/coaches/CoachForm.vue';
 import BaseCard from '@/components/ui/BaseCard.vue';
 
 export default {
-    components:{CoachForm, BaseCard}
+    components:{CoachForm, BaseCard},
+    methods:{
+        saveData(data){
+            this.$store.dispatch('coaches/registerCoach', data);
+            this.$router.replace('/coaches');
+        }
+    }
     
 }
 </script>
