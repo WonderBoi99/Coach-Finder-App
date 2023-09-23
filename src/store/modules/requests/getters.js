a4c76d8b8cd5d7bbcd4f76fc6a3b8daf8a237602
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2023-09-22 20:26:02 -0600

    fix router bug

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,8 +1,6 @@
 export default{
-    requests(state, _, _2, rootGetters){
-        const coachId = rootGetters.userId;
-        console.log(state.requests);
-        return state.requests.filter(req => req.coachId === coachId);
+    requests(state){
+        return state.requests;
     },
     hasRequests(_, getters){
         return getters.requests && getters.requests.length > 0;
